d1141c66319de9338b0048c855b2729d7a1e6e79
hiroki <confgaic@gmail.com>
2020-05-02 05:29:49 +0000

    PHP/Laravel 11

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -1,9 +1,10 @@
 <?php
-
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
+
 
 class ProfileController extends Controller
 {
--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,17 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
 
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>create.blade</h1>
-    </body>
-</html>
+
+{{-- profile.blade.phpの@yield('title')に'My Profile'を埋め込む --}}
+@section('title', 'My Profile')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>New content</h2>
+            </div>
+        </div>
+    </div>
+@endsection
