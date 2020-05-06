d0c73b8b92f82beeb90767952d952c5a11ede91c
hiroki <confgaic@gmail.com>
2020-05-06 01:28:20 +0000

    PHP/Laravel 12

--- routes/web.php
+++ routes/web.php
@@ -15,21 +15,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 
 
-//課題３
+
 //XXXにアクセスが来たら、コントローラーAAAのアクションbbbに渡す設定
 Route::get('XXX', 'AAA@bbb');
 
 
 
-//課題４
+//課題3  課題4
 //admin/profile/create にアクセスしたら ProfileController の add Actionに渡す設定
-Route::get('admin/profile/create', 'Admin\ProfileController@add');
+//追記、ログインしていない時にログイン画面にリダイレクトされる
+Route::get('admin/profile/create', 'Admin\ProfileController@add')->middleware('auth');
 
 //admin/profile/edit にアクセスしたら ProfileController の edit Actionに渡す設定
-Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+//追記、ログインしていない時にログイン画面にリダイレクトされる
+Route::get('admin/profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
