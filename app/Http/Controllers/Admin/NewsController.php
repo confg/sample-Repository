d7933ad2c9735ecbc4ab3febcfb73f5a3c255995
hiroki <confgaic@gmail.com>
2020-05-14 07:43:01 +0000

    プロフィール編集できる状態

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,42 +6,88 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\News;
 
-
 class NewsController extends Controller
 {
-   public function add()
+  public function add()
   {
-      return view('admin.news.create');
+    return view('admin.news.create');
   }
   
   public function create(Request $request)
   {
-     // 以下を追記
-      // Varidationを行う
+    // Varidationを行う
+    $this->validate($request, News::$rules);
+    
+    $news = new News;
+    $form = $request->all();
+    
+    // formに画像があれば、保存する
+    if ($form['image']) {
+    $path = $request->file('image')->store('public/image');
+    $news->image_path = basename($path);
+    } else {
+    $news->image_path = null;
+    }
+    unset($form['_token']);
+    unset($form['image']);
+    
+    // データベースに保存する      
+    $news->fill($form);
+    $news->save();
+    return redirect('admin/news/create');
+  }  
+  
+  public function index(Request $request)
+  {
+    $cond_title = $request->cond_title;
+    if ($cond_title != '') {
+      $posts = News::where('title', $cond_title)->get();
+    } else {
+      $posts = News::all();
+    }
+    return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+
+  
+  public function edit(Request $request)
+  {
+    // News Modelからデータを取得する
+    $news = News::find($request->id);
+  
+    return view('admin.news.edit', ['news_form' => $news]);
+  }
+
+
+  public function update(Request $request)
+  {
+    
+      // Validationをかける
       $this->validate($request, News::$rules);
-      
-      $news = new News;
-      $form = $request->all();
-      
-      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-      if (isset($form['image'])) {
+      // News Modelからデータを取得する
+      $news = News::find($request->id);
+      // 送信されてきたフォームデータを格納する
+      $news_form = $request->all();
+      if (isset($news_form['image'])) {
         $path = $request->file('image')->store('public/image');
         $news->image_path = basename($path);
-      } else {
-         $news->image_path = null;
+        unset($news_form['image']);
+      } elseif (isset($request->remove)) {
+        $news->image_path = null;
+        unset($news_form['remove']);
       }
-      
-      // フォームから送信されてきた_tokenを削除する
-      unset($form['_token']);
-      // フォームから送信されてきたimageを削除する
-      unset($form['image']);
-      
-      
-      // データベースに保存する
-      $news->fill($form);
-      $news->save();
-      
-      // admin/news/createにリダイレクトする
-      return redirect('admin/news/create');
-   }  
+      unset($news_form['_token']);
+      // 該当するデータを上書きして保存する
+      $news->fill($news_form)->save();
+
+      return redirect('admin/news/');
+  }
+  
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $news = News::find($request->id);
+      // 削除する
+      $news->delete();
+      return redirect('admin/news/');
+  }  
 }
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -16,22 +16,46 @@ class ProfileController extends Controller
     public function create(Request $request)
     {
         $this->validate($request, Profile::$rules);
+        
+        $news = new Profile;
+        $form = $request->all();
+        
+        $news->fill($form);
+        $news->save();
         return redirect('admin/profile/create');
         
     }
     
-    public function edit()
+    public function index(Request $request)
+  {
+    $cond_title = $request->cond_title;
+    if ($cond_title != '') {
+      $posts = Profile::where('title', $cond_title)->get();
+    } else {
+      $posts = Profile::all();
+    }
+    return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+  }
+    
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $news = Profile::find($request->id);
+        return view('admin.profile.edit' ,['news_form' => $news]);
+    }
+    
+    public function update(Request $request)
+    {
+        $this->validate($request, Profile::$rules);
         
-       
+        $news = Profile::find($request->id);
+        
+        $news_form = $request->all();
+        
+        unset($news_form['_token']);
+        
+        $news->fill($news_form)->save();
+        
+        return redirect('admin/profile/');
     }
-    
-    public function update()
-    {
-        return redirect('admin/profile/edit');
-    }
-    
-    
     
 }
